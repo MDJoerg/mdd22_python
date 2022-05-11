828bc6e5b1f386d2c3c99de22ce0d3e76d0d151c
JMR <joerg.mueller@ba-gmbh.com>
2022-05-11 12:59:56 +0200

    odata simple call

---
+++ demo_sap_odata_get_simple.py
@@ -0,0 +1,37 @@
+# Demo to connect to SAP ABAP Push Channel as Python WebSocket Client 
+# and listen to external commands
+# Magdeburger Developement Days 2022 - https://md-devdays.de
+# Joerg Mueller - @MDJoerg
+
+import sap_context
+import requests
+import json
+
+# build service url
+odata_url = "http://" + sap_context.sap_host + ":" + sap_context.sap_http_port
+odata_url += sap_context.sap_odata_endpoint + "/" + sap_context.sap_odata_flight_schedule_world + sap_context.sap_odata_params
+print("OData URL: ", odata_url)
+
+# call sap odata service api
+response = requests.get(odata_url)
+
+if response.status_code != 200:
+    print(f"Error {response.status_code}: {response.text}")
+else:
+    if response.text.find("{") != 0:
+        print("Response:", response.text)
+        print("Response is not a JSON answer?!")
+    else:    
+        json_response = json.loads(response.text)
+        #print(json_response)
+        print("JSON output detected")
+        json_results = json_response["d"]["results"]
+        print("============================")
+        index = 0
+        for json_result in json_results:
+            index += 1
+            print("\nResult Index: ", index)
+            for json_attribute in json_result:
+                print(json_attribute, " = ", json_result[json_attribute])
+
+
--- sap_context.py
+++ sap_context.py
@@ -1,11 +1,16 @@
 
 # define sap context
 sap_host = "10.17.1.12"
-sap_http_port = "8000"
-sap_https_port = "8000"
+sap_http_port = "80"
+sap_https_port = "443"
 sap_client = "100"
 sap_user = "USER"
 sap_pass = "PASS"
+
+# define OData context
+sap_odata_params   = "?sap-client=100&$format=json"
+sap_odata_endpoint = "/demos/mdd22/odata_srv"
+sap_odata_flight_schedule_world = "ZC_MDD_VDMF_FS_WLD"
 
 # define web socket context
 sap_wsock_protocol = "ws://"
